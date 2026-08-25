dd5d15947c2706a33177db8fa1ace5b2d3706442
jam bou <bouda.jamal@gmail.com>
2026-08-25 14:49:28 +0200

    modif accueil

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World --2026-- je m'entraîne");
+        System.out.println("Hello World --2026-- je m'entraîne en Java");
     }
 }
